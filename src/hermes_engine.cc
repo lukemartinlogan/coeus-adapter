4c8dc82631d7a9af9b89134cd51e22e48816bd0d
hxu65 <hxu40@hawk.iit.edu>
2024-11-04 09:38:00 -0600

    remove the cout

--- src/hermes_engine.cc
+++ src/hermes_engine.cc
@@ -602,7 +602,7 @@ void HermesEngine::PutDerived(adios2::core::VariableDerived variable,
     for (auto count: variable.m_Count) {
         total_count *= count;
     }
-    int numberOfProcesses = 4;
+    int numberOfProcesses = 2;
     Hermes->bkt->Put(name, total_count * sizeof(T), values);
 
     T* values2 = new T[total_count];
