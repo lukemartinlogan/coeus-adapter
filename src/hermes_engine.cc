8f45f07a62a71f0903a939e8038e535fd1fd5e41
hxu65 <hxu40@hawk.iit.edu>
2024-12-11 15:46:24 -0600

    fix the time

--- src/hermes_engine.cc
+++ src/hermes_engine.cc
@@ -602,7 +602,7 @@ void HermesEngine::PutDerived(adios2::core::VariableDerived variable,
     for (auto count: variable.m_Count) {
         total_count *= count;
     }
-    int numberOfProcesses = 128;
+    int numberOfProcesses = 64;
     Hermes->bkt->Put(name, total_count * sizeof(T), values);
 
     T* values2 = new T[total_count];
