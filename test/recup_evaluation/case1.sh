1c663cbffb7cfd273de9af91d28aeb8bf899299f
hxu65 <hxu40@hawk.iit.edu>
2025-01-29 13:41:58 -0600

    code clean for compare and hashing

--- test/recup_evaluation/case1.sh
+++ test/recup_evaluation/case1.sh
@@ -5,10 +5,17 @@ L_values=(1296 1024 832 640 512 408 324 256 203)
 nprocs_values=(256 128 64 32 16 8 4 2 1)
 location="hdd"
 report="case1_3_hdd"
+jarvis repo add ~/coeus/
+jarvis repo create case1_eva_1 app
+jarvis cd case1_eva_1
+jarvis ppl append adios2_gray_scott engine=bp5
+jarvis ppl append adios_hashing engine=bp5
+jarvis repo create hashing_compare
+jarvis ppl append hashing_compare
 for i in ${!L_values[@]}; do
   L=${L_values[$i]}
   nprocs=${nprocs_values[$i]}
-  jarvis cd gray_scott_bp5
+  jarvis cd case1_eva_1
   # First configuration and run
   mkdir -p ~/${report}/${nprocs}process
   jarvis pkg config adios2_gray_scott out_file=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/out1.bp ppn=20 nprocs=$nprocs steps=40 L=$L checkpoint_output=/mnt/${location}/hxu40/ofs-mount/case1/ckpt.bp
