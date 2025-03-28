3abb3665349434db516ab9792f599f42193127ee
lukemartinlogan <lukemartinlogan@gmail.com>
2025-03-27 23:08:54 -0500

    Local hash breaks for pool creation, use global only

--- src/hermes_engine.cc
+++ src/hermes_engine.cc
@@ -116,13 +116,11 @@ void HermesEngine::Init_() {
 
       // add rank with consensus
       HILOG(kInfo, "Starting to create rank consensus");
-      // rank_consensus.Create(HSHM_MCTX,
-      //                       chi::DomainQuery::GetDirectHash(
-      //                           chi::SubDomainId::kGlobalContainers, 0),
-      //                       chi::DomainQuery::GetGlobalBcast(),
-      //                       "rankConsensus");
-      rank_consensus.Create(HSHM_MCTX, chi::DomainQuery::GetLocalHash(0),
-                            chi::DomainQuery::GetLocalHash(0), "rankConsensus");
+      rank_consensus.Create(HSHM_MCTX,
+                            chi::DomainQuery::GetDirectHash(
+                                chi::SubDomainId::kGlobalContainers, 0),
+                            chi::DomainQuery::GetGlobalBcast(),
+                            "rankConsensus");
       HILOG(kInfo, "Created rank consensus: {}", rank_consensus.id_);
       rank =
           rank_consensus.GetRank(HSHM_MCTX, chi::DomainQuery::GetLocalHash(0));
